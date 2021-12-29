ba03c309b7d8afdae50cb8d734d7bf2a8a242e67
carolinabezares <201353@ids.upchiapas.edu.mx>
2021-12-28 21:35:33 -0600

    cambios a usuario

--- index.js
+++ index.js
@@ -48,8 +48,50 @@ function usuarios(){
 }
 
 function aspirantes(){
+    const nombre = document.getElementById("nombre").value;
+    const apellidopaterno = document.getElementById("paterno").value;
+    const apellidomaterno = document.getElementById("materno").value;
+    const telefonoprincipal = document.getElementById("telefono_principal").value;
+    const telefonosecundario = document.getElementById("telefono_secundario").value;
+    const correo = document.getElementById("correo").value;
+    const domicilio = document.getElementById("domicilio").value;
+    const alergias = document.getElementById("alergia").value;
+    const padecimientos = document.getElementById("padecimiento").value;
 
+    const nombrefactura = document.getElementById("nombre_factura").value;
+    const paternofactura = document.getElementById("paterno_factura").value;
+    const maternofactura = document.getElementById("materno_factura").value;
+    const codigopostal = document.getElementById("codigo_postal").value;
+    const colonia = document.getElementById("colonia").value;
+    const nexterno = document.getElementById("no_ext").value;
+    const ninterno = document.getElementById("no_int").value;
+    const rfc = document.getElementById("id_rfc").value;
 
+    axios.post("http://localhost:8000/aspirantes", {
+        nombre1 = nombre,
+        apellidopaterno1 = apellidopaterno,
+        apellidomaterno1 = apellidomaterno,
+        telefonoprincipal1 = telefonoprincipal,
+        telefonosecundario1 = telefonosecundario,
+        correo1 = correo,
+        domicilio1 = domicilio,
+        alergias1 = alergias,
+        padecimientos1 = padecimientos,
+
+        nombrefactura1 = nombrefactura, 
+        paternofactura1 = paternofactura,
+        maternofactura1 = maternofactura,
+        codigopostal1 = codigopostal,
+        colonia1 = colonia,
+        nexterno1 = nexterno,
+        ninterno1 = ninterno,
+        rfc1 = rfc
+        
+    }).then((data) => {
+        swal("Buen trabajo!", "Ha ingresado con exito!", "success");
+        console.log(data);
+        console.log("registro")
+    }).catch(e => { console.log("El ingreso fallo"); })
 }
 
 function interesados(){
